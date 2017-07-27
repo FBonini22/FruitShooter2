b367301fc06d97f68d0424a666d317d4bc8326f0
jalevis <30274579+jalevis@users.noreply.github.com>
2017-07-27 19:26:29 -0400

    Hitbox code line for line from video
    V 0.1a

--- src/entities/HitBox.java
+++ src/entities/HitBox.java
@@ -2,18 +2,50 @@ package entities;
 
 public abstract class HitBox {
 
-	//Constants
+	public float x;
+	public float y;
+	public float width;
+	public float height;
 	
-	
-	//Instance Variables
-	//TO-DO change accessibility
-	protected float x;
-	protected float y;
-	protected float width;
-	protected float height;
-	
-	public HitBox(){
-		
+	public HitBox() {
 	}
 	
+	public HitBox(float x, float y, float width, float height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		System.out.println(this.x);
+		System.out.println(this.y);
+		System.out.println(this.width);
+		System.out.println(this.height);
+	}
+	
+	public float getEndX() {
+		return (x + width);
+	}
+	
+	public float getEndY() {
+		return (y + height);
+	}
+	
+	public float getHalfWidth() {
+		return (width / 2);
+	}
+	
+	public float getHalfHeight() {
+		return (height / 2);
+	}
+	
+	public float getCenterX() {
+		return (x + getHalfWidth());
+	}
+	
+	public float getCenterY() {
+		return (y + getHalfHeight());
+	}
+	
+	public boolean hitTest(HitBox b) {
+		return (b.getEndX() >= x && b.getEndY() >= y && getEndX() >= b.x && getEndY() >= b.y);
+	}
 }
