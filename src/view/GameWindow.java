46cc0d0e71cd98654e303716a0db9fcc5c98b34d
FBonini22 <30274597+FBonini22@users.noreply.github.com>
2017-07-27 20:52:00 -0400

    Update Files

--- src/view/GameWindow.java
+++ src/view/GameWindow.java
@@ -3,7 +3,7 @@ package view;
 public class GameWindow {
 	
 	//Constants
-	public static final int SCREEN_WIDTH = 600;			//Width of the game screen in pixels
-	public static final int SCREEN_HEIGHT = 800;		//Height of the game screen in pixels
+	public static final int SCREEN_WIDTH = 800;			//Width of the game screen in pixels
+	public static final int SCREEN_HEIGHT = 600;		//Height of the game screen in pixels
 
 }
