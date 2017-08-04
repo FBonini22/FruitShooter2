9f6df48c583acc12a8d4e70bfa17e49a365096ab
FBonini22 <30274597+FBonini22@users.noreply.github.com>
2017-08-04 15:42:42 -0400

    Initial Audio Commit
    Added audio for Player hit and player fire.
    
    Must enable extra packages to use this branch.

---
+++ src/sounds/FX.java
@@ -0,0 +1,12 @@
+package sounds;
+
+import java.net.URL;
+
+import org.newdawn.slick.Sound;
+
+public class FX{
+
+	public static final String PLAYER_FIRE = "sound/PlayerFire.ogg";
+	public static final String PLAYER_HIT = "sound/PlayerHit.ogg";
+	
+}
